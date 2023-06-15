93269c857295e389c57ad8d560fb608dc44e37f0
Ferdinand Estoque <festoqufx@gmail.com>
2023-06-15 10:00:06 +0800

    1
    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Contact from './components/contact/Contact';
-// import Experience from './components/experience/Experience';
 import Footer from './components/footer/Footer';
 import Svg from './components/svg/Svg';
 import Header from './components/header/Header';
